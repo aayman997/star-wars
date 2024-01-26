0e708ae9df27c4e101e39c49f09c9432e10381f2
Ahmed Ayman <ahmed1997164@gmail.com>
2024-01-26 17:00:43 +0200

    Project clean-up Remove template code configure eslint fix ts-paths issue with vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
+import "./globals.scss";
+import App from "@src/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
