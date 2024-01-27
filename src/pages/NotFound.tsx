0f71b143283c26d63f032f0d4206e3e00d8d3114
Ahmed Ayman <ahmed1997164@gmail.com>
2024-01-27 23:33:13 +0200

    Add page not found layout
    Took 2 minutes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,34 @@
+import { HiChevronLeft } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+	const navigate = useNavigate();
+
 	return (
-		<div>
-			Page Not Found
-		</div>
+		<>
+			<style>
+				{`
+			        #root {
+			          height: 100dvh;
+			        }
+		      `}
+			</style>
+			<div className="container mx-auto flex h-full w-full items-center justify-center text-center">
+				<div className="flex flex-col items-center justify-center gap-y-4 text-center">
+					<div className="mt-10">
+						<h1>The page you are looking for could not be found 😢</h1>
+						<button
+							type="button"
+							onClick={() => navigate("/", { replace: true })}
+							className="text mx-auto mt-4 flex items-center justify-center gap-1 rounded-md border border-brand-500/50 bg-white px-4 py-2
+							font-bold text-brand-500 shadow-2xl"
+						>
+							<HiChevronLeft />
+							Go to Homepage
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
